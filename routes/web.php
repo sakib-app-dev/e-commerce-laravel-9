a0c4b8838aa8db8a105ae7385c6940c2214b7ee7
sakib-ahamed <saapurbo09@gmail.com>
2022-10-27 23:11:21 +0600

    User Frontend Added

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,13 +17,50 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+
+
+
+
+Route::get('/', [UserController::class, 'home'])->name('home');
+Route::get('/about', [UserController::class, 'about'])->name('about');
+
+Route::prefix('/users')->name('users.')->group(function(){
+
+
+    
+    Route::get('/checkout', [UserController::class, 'checkout'])->name('checkout');
+    Route::get('/invoice', [UserController::class, 'invoice'])->name('invoice');
+
+    
+    Route::prefix('/prouct')->name('product.')->group(function(){
+        Route::get('/list', [UserController::class, 'productList'])->name('list');
+        Route::get('/detail', [UserController::class, 'productDetail'])->name('detail');
+    });
+
+    Route::get('/thank-you', [UserController::class, 'thankYou'])->name('thank-you');
+    // Route::get('/login', [UserController::class, 'login'])->name('login');
+    // Route::get('/register', [UserController::class, 'register'])->name('register');
+
+
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+
+
+
+
+
+
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
 
